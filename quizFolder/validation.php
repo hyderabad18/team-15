91b34e845856d72133980e049512e1072f7b3c6b
varshini <varshinichowdary4@gmail.com>
2018-07-15 06:23:51 +0530

    AfterValidation

---
+++ quizFolder/validation.php
@@ -0,0 +1,39 @@
+<?php 
+$count=0;
+if(isset($_POST['submit']))
+{
+if(isset($_POST['customRadio'])){
+$val=$_POST['customRadio'];
+if($val=="correct")
+{$count++;
+
+}}
+ if(isset($_POST['customRadio3'])){
+$val=$_POST['customRadio3'];
+
+if($val=="correct1")
+{
+	$count++;
+
+}
+ }
+  if(isset($_POST['customRadio1'])){
+$val=$_POST['customRadio1'];
+
+if($val=="correct2")
+{$count++;
+
+}
+ }
+  if(isset($_POST['customRadio2'])){
+$val=$_POST['customRadio2'];
+
+if($val=="correct3")
+{$count++;
+
+}
+ }
+ echo "$count";
+}
+
+ ?>
