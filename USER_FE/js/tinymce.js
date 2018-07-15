b6bfb9f281c85b59cac457a94208cbcc03ed2336
Suraj B <surajbodapati97@gmail.com>
2018-07-15 08:38:11 +0530

    front end for showing jobs

---
+++ USER_FE/js/tinymce.js
@@ -0,0 +1,40 @@
+tinymce.init({
+	    selector: '#texteditor',
+	    plugins:'code image',
+	    toolbar: 'undo redo | styleselect | bold italic | link image',
+
+    // without images_upload_url set, Upload tab won't show up
+    images_upload_url: 'uploadImage.php',
+    
+    // override default upload handler to simulate successful upload
+    images_upload_handler: function (blobInfo, success, failure) {
+        var xhr, formData;
+      
+        xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+        xhr.open('POST', 'uploadImage.php');
+      
+        xhr.onload = function() {
+            var json;
+        
+            if (xhr.status != 200) {
+                failure('HTTP Error: ' + xhr.status);
+                return;
+            }
+        
+            json = JSON.parse(xhr.responseText);
+        
+            if (!json || typeof json.location != 'string') {
+                failure('Invalid JSON: ' + xhr.responseText);
+                return;
+            }
+        
+            success(json.location);
+        };
+      
+        formData = new FormData();
+        formData.append('file', blobInfo.blob(), blobInfo.filename());
+      
+        xhr.send(formData);
+    },
+});
